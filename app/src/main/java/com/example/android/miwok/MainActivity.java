cc0371fb72945d0d8a25565df57c542ac73736d3
leoybkim <leoybkim@gmail.com>
2016-12-06 23:46:47 -0800

    Implement ViewPager! so cool

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.android.miwok;
 
-import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,45 +11,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Set a click listener on numbers textview
-        TextView tv_numbers = (TextView) findViewById(R.id.numbers);
-        tv_numbers.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent numbersIntent = new Intent (MainActivity.this, NumbersActivity.class);
-                startActivity(numbersIntent);
-            }
-        });
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
-        // Set a click listener on family textview
-        TextView tv_family = (TextView) findViewById(R.id.family);
-        tv_family.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent familyIntent = new Intent (MainActivity.this, FamilyActivity.class);
-                startActivity(familyIntent);
-            }
-        });
+        // Create an adapter that knows which fragment should be shown on each page
+        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
 
-        // Set a click listener on family textview
-        TextView tv_colors = (TextView) findViewById(R.id.colors);
-        tv_colors.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent colorsIntent = new Intent (MainActivity.this, ColorsActivity.class);
-                startActivity(colorsIntent);
-            }
-        });
-
-        // Set a click listener on family textview
-        TextView tv_phrases = (TextView) findViewById(R.id.phrases);
-        tv_phrases.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent phrasesIntent = new Intent (MainActivity.this, PhrasesActivity.class);
-                startActivity(phrasesIntent);
-            }
-        });
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
     }
 
 }
